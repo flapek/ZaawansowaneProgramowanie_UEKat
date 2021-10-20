4b6121788e3f7f1693cf359908298a5b05a2b8a6
Flapek <filap4@gmail.com>
2021-10-20 18:45:49 +0200

    Update browary.py

--- lab3/browary.py
+++ lab3/browary.py
@@ -1,30 +1,47 @@
 import json
+from collections import namedtuple
 import requests
 
-class Browar:
-    def __init__(self, j):
-        self.id = j.get('id'),
-        self.name = j.get('name'),
-        self.brewery_type = j.get('brewery_type'),
-        self.street = j.get('street'),
-        self.address_2 = j.get('address_2'),
-        self.address_3 = j.get('address_3'),
-        self.city = j.get('city'),
-        self.state = j.get('state'),
-        self.county_province = j.get('county_province'),
-        self.postal_code = j.get('postal_code'),
-        self.country = j.get('country'),
-        self.longitude = j.get('longitude'),
-        self.latitude = j.get('latitude'),
-        self.phone = j.get('phone'),
-        self.website_url = j.get('website_url'),
-        self.updated_at = j.get('updated_at'),
-        self.created_at = j.get('created_at')
+URL = 'https://api.openbrewerydb.org/breweries'
+
+
+class Browar(object):
+    def __init__(self, json: str):
+        self.id = json['id'],
+        self.name = json['name'],
+        self.brewery_type = json['brewery_type'],
+        self.street = json['street'],
+        self.address_2 = json['address_2'],
+        self.address_3 = json['address_3'],
+        self.city = json['city'],
+        self.state = json['state'],
+        self.county_province = json['county_province'],
+        self.postal_code = json['postal_code'],
+        self.country = json['country'],
+        self.longitude = json['longitude'],
+        self.latitude = json['latitude'],
+        self.phone = json['phone'],
+        self.website_url = json['website_url'],
+        self.updated_at = json['updated_at'],
+        self.created_at = json['created_at']
 
     def __str__(self):
-        print(f'{self.id}, {self.name}, {self.brewery_type}')
+        return f'{self.id}, {self.name}, {self.brewery_type}'
 
-response = requests.get("https://api.openbrewerydb.org/breweries?page=20")
 
-result = Browar(response.json()[1])
-print(result)
+def customBrowarDecoder(browarDict):
+    return namedtuple('X', browarDict.keys())(*browarDict.values())
+
+
+params = {'by_city': None, 'per_page': 20}
+response = requests.get(URL, params=params)
+
+browar = json.loads(response.json()[0], object_hook=customBrowarDecoder)
+
+print(browar)
+
+# data = response.json()
+# print(data)
+
+# result = Browar(response.json()[0])
+# print(result)
