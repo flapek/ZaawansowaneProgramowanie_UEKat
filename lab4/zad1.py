defde792f4cfd2e4043153bbffcea3398265809f
flapek <filap4@gmail.com>
2021-10-26 13:56:55 +0200

    Create zad1.py

---
+++ lab4/zad1.py
@@ -0,0 +1,14 @@
+class Student: 
+    def __init__(self, name: str, marks: int):
+        self.name = name
+        self.marks = marks
+
+    def is_passed(self) -> bool:
+        return self.marks > 50
+
+
+student1 = Student(name="Student", marks=50)
+student2 = Student(name="Student", marks=59)
+
+print(f'Student1 zdał {student1.is_passed()}')
+print(f'Student2 zdał {student2.is_passed()}')
