779322f57a90faacf5cc90578c124dfff632f56d
flapek <filap4@gmail.com>
2021-11-03 14:47:56 +0100

    Update zad3.py

--- lab4/zad3.py
+++ lab4/zad3.py
@@ -5,19 +5,28 @@ class Property:
         self.price = price
         self.address = address
 
+
 class House(Property):
-    def __init__(self, area: int, rooms: int, price: float, address: str, fence: int):
+    def __init__(self, area: int, rooms: int, price: float,
+                 address: str, fence: int):
         super().__init__(area, rooms, price, address)
         self.fence = fence
+
     def __str__(self) -> str:
-        return f'House area {self.area}m2, numbers of rooms {self.rooms}, fence size {self.fence}, price {self.price}, address {self.address}.'
+        return f'House area {self.area}m2, numbers of rooms \
+            {self.rooms}, fence size {self.fence}, \
+                price {self.price}, address {self.address}.'
+
 
 class Flat(Property):
-    def __init__(self, area: int, rooms: int, price: float, address: str, floor: int):
+    def __init__(self, area: int, rooms: int, price: float,
+                 address: str, floor: int):
         super().__init__(area, rooms, price, address)
         self.floor = floor
+
     def __str__(self) -> str:
-        return f'Flat area {self.area}m2, numbers of rooms {self.rooms}, floor {self.floor}, price {self.price}, address {self.address}.'
+        return f'Flat area {self.area}m2, numbers of rooms {self.rooms}, \
+            floor {self.floor}, price {self.price}, address {self.address}.'
 
 
 flat = Flat(45, 3, 300000, 'Katowcie, Piłsudzkiego 200/45', 4)
