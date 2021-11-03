6b77833f7870ae8d99840cc82d506d413b400e0f
Flapek <filap4@gmail.com>
2021-11-03 20:35:00 +0100

    update formating

--- lab4/zad3.py
+++ lab4/zad3.py
@@ -55,6 +55,5 @@ class Flat(Property):
 
 flat = Flat(45, 3, 300000, 'Katowcie, Piłsudzkiego 200/45', 4)
 house = House(120, 6, 5500000, 'Katowcie, Piłsudzkiego 1', 100)
-
 print(flat)
 print(house)
