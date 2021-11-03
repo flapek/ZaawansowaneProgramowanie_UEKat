354f06b4133d91404fff13ef5efe4bf84e1f4cf0
Flapek <filap4@gmail.com>
2021-11-03 20:17:06 +0100

    define property

--- lab4/zad1.py
+++ lab4/zad1.py
@@ -1,14 +1,21 @@
 class Student:
+    @property
+    def name(self) -> None:
+        return self._name
+
+    @property
+    def marks(self) -> None:
+        return self._marks
+
     def __init__(self, name: str, marks: int):
-        self.name = name
-        self.marks = marks
+        self._name = name
+        self._marks = marks
 
     def is_passed(self) -> bool:
-        return self.marks > 50
+        return self._marks > 50
 
 
 student1 = Student(name="Student", marks=50)
 student2 = Student(name="Student", marks=59)
-
 print(f'Student1 zdał {student1.is_passed()}')
 print(f'Student2 zdał {student2.is_passed()}')
