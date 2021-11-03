0f68f6f4ed6de60f434c015b8e73fbeb9faca5bc
Flapek <filap4@gmail.com>
2021-11-03 20:31:35 +0100

    Update zad2.py

--- lab4/zad2.py
+++ lab4/zad2.py
@@ -1,82 +1,174 @@
 class Student:
+    @property
+    def name(self) -> None:
+        return self._name
+
+    @property
+    def marks(self) -> None:
+        return self._marks
+
     def __init__(self, name: str, marks: int):
-        self.name = name
-        self.marks = marks
+        self._name = name
+        self._marks = marks
 
     def is_passed(self) -> bool:
-        return self.marks > 50
+        return self._marks > 50
 
     def __str__(self) -> str:
-        return f'Student name: {self.name}. Student mark: {self.marks}%'
+        return f'Student name: {self._name}. Student mark: {self._marks}%'
 
 
 class Library:
+    @property
+    def city(self) -> None:
+        return self._city
+
+    @property
+    def street(self) -> None:
+        return self._street
+
+    @property
+    def zip_code(self) -> None:
+        return self._zip_code
+
+    @property
+    def open_hours(self) -> None:
+        return self._open_hours
+
+    @property
+    def phone(self) -> None:
+        return self._phone
+
     def __init__(self, city: str, street: str, zip_code: str,
                  open_hours: str, phone: str):
-        self.city = city
-        self.street = street
-        self.zip_code = zip_code
-        self.open_hours = open_hours
-        self.phone = phone
+        self._city = city
+        self._street = street
+        self._zip_code = zip_code
+        self._open_hours = open_hours
+        self._phone = phone
 
     def __str__(self) -> str:
-        return f'Library is located in {self.city} (zip code {self.zip_code}) \
-            on the street {self.street}. Open houers {self.open_hours}, \
-                phone number {self.phone}.'
+        return f'Library is located in {self._city} (zip code {self._zip_code}) \
+            on the street {self._street}. Open houers {self._open_hours}, \
+                phone number {self._phone}.'
 
 
 class Employee:
+    @property
+    def first_name(self) -> None:
+        return self._first_name
+
+    @property
+    def last_name(self) -> None:
+        return self._last_name
+
+    @property
+    def hire_date(self) -> None:
+        return self._hire_date
+
+    @property
+    def birth_date(self) -> None:
+        return self._birth_date
+
+    @property
+    def city(self) -> None:
+        return self._city
+
+    @property
+    def zip_code(self) -> None:
+        return self._zip_code
+
+    @property
+    def phone(self) -> None:
+        return self._phone
+
     def __init__(self, first_name: str, last_name: str, hire_date: str,
                  birth_date: str, city: str, street: str, zip_code: str,
                  phone: str):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.hire_date = hire_date
-        self.birth_date = birth_date
-        self.city = city
-        self.street = street
-        self.zip_code = zip_code
-        self.phone = phone
+        self._first_name = first_name
+        self._last_name = last_name
+        self._hire_date = hire_date
+        self._birth_date = birth_date
+        self._city = city
+        self._street = street
+        self._zip_code = zip_code
+        self._phone = phone
 
     def __str__(self) -> str:
-        return f'Employee data: \n- first name: {self.first_name}, \n- last \
-            name: {self.last_name}, \n- hire date: {self.hire_date}, \n\
-                - birth date: {self.birth_date}, \n- city: {self.city}, \n- \
-                    street: {self.street}, \n- zip code: {self.zip_code}, \n- \
-                        phone: {self.phone},'
+        return f'Employee data: \n- first name: {self._first_name}, \n- last \
+            name: {self._last_name}, \n- hire date: {self._hire_date}, \n\
+                - birth date: {self._birth_date}, \n- city: {self._city}, \n- \
+                    street: {self._street}, \n- zip code: {self._zip_code}, \n- \
+                        phone: {self._phone},'
 
 
 class Book:
+    @property
+    def library(self) -> None:
+        return self._library
+
+    @property
+    def publication_date(self) -> None:
+        return self._publication_date
+
+    @property
+    def author_name(self) -> None:
+        return self._author_name
+
+    @property
+    def author_surname(self) -> None:
+        return self._author_surname
+
+    @property
+    def number_of_pages(self) -> None:
+        return self._number_of_pages
+
     def __init__(self, library: Library, publication_date: str,
                  author_name: str, author_surname: str, number_of_pages: int):
-        self.library = library
-        self.publication_date = publication_date
-        self.author_name = author_name
-        self.author_surname = author_surname
-        self.number_of_pages = number_of_pages
+        self._library = library
+        self._publication_date = publication_date
+        self._author_name = author_name
+        self._author_surname = author_surname
+        self._number_of_pages = number_of_pages
 
     def __str__(self) -> str:
-        return f'Book is located in library: {self.library}. \
-            Book information:\n- publication date: {self.publication_date}\n\
-                - author name: {self.author_name}\n- author surname: \
-                    {self.author_surname}\n- number of pages: \
-                        {self.number_of_pages}'
+        return f'Book is located in library: {self._library}. \
+            Book information:\n- publication date: {self._publication_date}\n\
+                - author name: {self._author_name}\n- author surname: \
+                    {self._author_surname}\n- number of pages: \
+                        {self._number_of_pages}'
 
 
 class Order:
+    @property
+    def employee(self) -> None:
+        return self._employee
+
+    @property
+    def student(self) -> None:
+        return self._student
+
+    @property
+    def books(self) -> None:
+        return self._books
+
+    @property
+    def order_date(self) -> None:
+        return self._order_date
+
     def __init__(self, employee: Employee, student: Student,
-                 books: list[Book], order_date: str):
-        self.employee = employee
-        self.student = student
-        self.books = books
-        self.order_date = order_date
+                 books: "list[Book]", order_date: str):
+        self._employee = employee
+        self._student = student
+        self._books = books
+        self._order_date = order_date
 
     def __str__(self) -> str:
-        result = f'Employee taking the order\n{self.employee}\n\
-            from {self.student}\non books'
-        for book in self.books:
+        result = f'Employee taking the order\n{self._employee}\n\
+            from {self._student}\non books'
+        for book in self._books:
             result = result + f'\n- {book}'
-        result = result + f'. Order date {self.order_date}.\n\n'
+        result = result + f'. Order date {self._order_date}.\n\n'
         return result
 
 
