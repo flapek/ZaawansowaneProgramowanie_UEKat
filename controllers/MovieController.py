c8494f7886a6a3343affd7d0b68844a507ccaa1a
flapek <filap4@gmail.com>
2021-11-23 14:44:26 +0100

    move methods and classes

--- controllers/MovieController.py
+++ controllers/MovieController.py
@@ -1,6 +1,6 @@
 from flask_restful import Resource
+from reositories.movie_repository import parse
 from utils.file_reader import read_file
-from utils.movie_extension import parse
 from utils.json_extension import to_json, to_json_string
 
 
--- utils/json_extension.py
+++ utils/json_extension.py
@@ -3,7 +3,6 @@ import json
 
 def to_json_string(list_of_objects: list):
     obj = [o.__dict__() for o in list_of_objects]
-
     return json.dumps(obj, default=lambda obj: obj.__dict__)
 
 
